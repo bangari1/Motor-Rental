aacbe6229746946d4fb47f0fef83a242010343d0
Sagar Joshi <coderjoshi15@gmail.com>
2025-09-01 21:10:21 +0530

    Animation Added

--- Navigations.js
+++ Navigations.js
@@ -3,15 +3,26 @@ import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
+import { Platform } from 'react-native';
+import { enableScreens } from 'react-native-screens';
+
+// Only enable react-native-screens on native platforms
+if (Platform.OS === 'ios' || Platform.OS === 'android') {
+  enableScreens();
+}
+
+
 import SignIn from './screens/SignIn';
 import SignUp from './screens/SignUp';
+import SplashScreen from './screens/SplashScreen';
 
 const Stack = createNativeStackNavigator();
 
 export default function Navigation() {
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName="SignIn">
+      <Stack.Navigator initialRouteName="Splash" screenOptions={{ headerShown: false }}>
+        <Stack.Screen name="Splash" component={SplashScreen} />
         <Stack.Screen name="SignIn" component={SignIn} options={{ title: 'Sign In' }} />
         <Stack.Screen name="SignUp" component={SignUp} options={{ title: 'Sign Up' }} />
       </Stack.Navigator>
